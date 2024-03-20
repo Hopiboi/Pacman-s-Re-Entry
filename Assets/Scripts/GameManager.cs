8a4fdc0c527ef7edcf9f1d58f8d6a7e3ae47d4fd
Hopi <89178015+Hopiboi@users.noreply.github.com>
2024-03-21 03:29:44 +0800

    Re:Entry

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    [SerializeField] private GameObject[] ghost;
-    [SerializeField] private GameObject[] pacman;
+    [SerializeField] private Ghost[] ghost;
+    [SerializeField] private Pacman pacman;
     [SerializeField] private Transform pellets;
 
 
@@ -14,11 +14,46 @@ public class GameManager : MonoBehaviour
 
     void Start()
     {
-        
+        NewGame();
     }
 
-    void Update()
+    private void NewGame()
     {
-        
+        SetScore(0);
+        SetLives(3);
+        NewRound();
     }
+
+    //New round, starting the game or clearing all pellets
+    private void NewRound()
+    {
+        foreach (Transform pellet in this.pellets)
+        {
+            pellet.gameObject.SetActive(true);
+        }
+
+        ResetState();
+    }
+
+    //losing lives only, therefore, resetting the state
+    private void ResetState()
+    {
+        for (int i = 0; i < this.ghost.Length; i++)
+        {
+            this.ghost[i].gameObject.SetActive(true);
+        }
+
+        this.pacman.gameObject.SetActive(true);
+    }
+
+    private void SetScore(int score)
+    {
+        this.score = score;
+    }
+
+    private void SetLives(int lives)
+    {
+        this.lives = lives;
+    }
+
 }
