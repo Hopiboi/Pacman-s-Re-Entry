63a025ecf8bb7e3d0064f59c02a9327a1674fb1e
Hopi <89178015+Hopiboi@users.noreply.github.com>
2024-04-03 00:19:56 +0800

    Re:Entry

--- Assets/Scripts/AnimationScript.cs
+++ Assets/Scripts/AnimationScript.cs
@@ -8,9 +8,9 @@ public class AnimationScript : MonoBehaviour
 {
     public SpriteRenderer spr { get; private set; }
 
-    [SerializeField] private Sprite[] sprite;
+    [SerializeField] private Sprite[] sprites;
     [SerializeField] private bool looping = true;
-    [SerializeField] private float animationTime = .25f;
+    [SerializeField] private float animationTime = .125f;
     [SerializeField] public int animationFrame { get; private set; }
 
     private void Awake()
@@ -25,12 +25,31 @@ public class AnimationScript : MonoBehaviour
 
     private void Advance()
     {
+
+        if (!this.spr.enabled)
+        {
+            return;
+        }
+
+
         this.animationFrame++;
 
-        if (this.animationFrame >= this.sprite.Length && this.looping)
+        if (this.animationFrame >= this.sprites.Length && this.looping)
         {
             this.animationFrame = 0;
         }
+
+        if (this.animationFrame >= 0 && this.animationFrame < this.sprites.Length)
+        {
+            this.spr.sprite = this.sprites[this.animationFrame];
+        }
+    }
+
+    public void Restart()
+    {
+        this.animationFrame = -1;
+
+        Advance();
     }
 
     // Update is called once per frame
