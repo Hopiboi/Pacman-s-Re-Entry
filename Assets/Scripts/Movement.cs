db4aecaa576b87d7cd619c57b38193dcf57311b9
Hopi <89178015+Hopiboi@users.noreply.github.com>
2024-04-06 00:26:30 +0800

    Re:Entry

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -2,12 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class Movement : MonoBehaviour
 {
+    [Header("Speed Setting")]
+    [SerializeField] private float speed = 8f;
+    [SerializeField] private float speedMultiplier = 8f;
+
+    [Header("Direction and Collision")]
+    [SerializeField] private Vector2 initialDirection;
+    [SerializeField] private LayerMask obstacleLayer;
+
+    //[Header("Player Direction")]
+    [SerializeField] public Vector2 direction { get; private set; }
+    [SerializeField] public Vector2 nextDirection { get; private set; }
+
+    public Rigidbody2D rg2D { get; private set; }
+
+
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        rg2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
