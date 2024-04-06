a001b2a46087afd8fdde97425b851e33dede8791
Hopi <89178015+Hopiboi@users.noreply.github.com>
2024-04-07 00:03:02 +0800

    Re:Entry

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -16,17 +16,24 @@ public class Movement : MonoBehaviour
     //[Header("Player Direction")]
     [SerializeField] public Vector2 direction { get; private set; }
     [SerializeField] public Vector2 nextDirection { get; private set; }
+    [SerializeField] public Vector3 startingPosition { get; private set; }
 
     public Rigidbody2D rg2D { get; private set; }
 
 
-    // Start is called before the first frame update
-    void Awake()
+    
+    private void Awake()
     {
         rg2D = GetComponent<Rigidbody2D>();
+        this.startingPosition = this.transform.position;
     }
 
-    // Update is called once per frame
+    private void Start()
+    {
+        
+    }
+
+
     void Update()
     {
         
