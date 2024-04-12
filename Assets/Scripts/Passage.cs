c41b19d0244091bf88cbb413ab5795ce9abb4b01
Hopi <89178015+Hopiboi@users.noreply.github.com>
2024-04-13 00:59:44 +0800

    Re:Entry

--- Assets/Scripts/Passage.cs
+++ Assets/Scripts/Passage.cs
@@ -4,15 +4,13 @@ using UnityEngine;
 
 public class Passage : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Transform connection;
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
-    }
+        Vector3 position = collision.transform.position;
+        position.x = this.connection.position.x; //this object position
+        position.y = this.connection.position.y; //this object position
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        collision.transform.position = position; //Moving the object on that position
     }
 }
