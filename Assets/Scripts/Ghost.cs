4737f770dd0cbdebe42193432bde4f29a704a01c
Hopi <89178015+Hopiboi@users.noreply.github.com>
2024-04-24 02:16:39 +0800

    Re:Entry

--- Assets/Scripts/Ghost.cs
+++ Assets/Scripts/Ghost.cs
@@ -5,7 +5,14 @@ using UnityEngine;
 public class Ghost : MonoBehaviour
 {
     public int points = 200;
+    public Movement movement { get; private set; }
+    public GhostSpawn spawn { get; private set; }
+    public GhostScatter scatter { get; private set; }
+    public GhostChase chase { get; private set; }
+    public GhostFrightened frightened { get; private set; }
 
+    public GhostBehavior initialBehavior;
+    public Transform target;
 
     void Start()
     {
