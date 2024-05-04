ca47746e5f49e7b42a62efc0059e2c19f947df55
Hopi <89178015+Hopiboi@users.noreply.github.com>
2024-05-05 01:37:09 +0800

    Re:Entry

--- Assets/Scripts/GhostChase.cs
+++ Assets/Scripts/GhostChase.cs
@@ -19,20 +19,21 @@ public class GhostChase : GhostBehavior
             Vector2 direction = Vector2.zero; // Starting point to find pacman using 0,0
             float minDistance = float.MaxValue; // distance to pacman
 
+            //Looping
             foreach (Vector2 availableDirection in node.availableDirections)
             {
-                Vector3 newPosition = this.transform.position + new Vector3(availableDirection.x, availableDirection.y, 0f); // ghost position
-                float distance = (this.ghost.target.position - newPosition).sqrMagnitude; // ghost position - pacman position
+                Vector3 newPosition = this.transform.position + new Vector3(availableDirection.x, availableDirection.y, 0f); // ghost position + new position
+                float distance = (this.ghost.target.position - newPosition).sqrMagnitude; // ghost position - pacman position = distance needed // optimizing the route
 
-                //
-                if (distance < minDistance)
+                // Direction to bring closer to pacman than before
+                if (minDistance > distance)
                 {
-                    direction = availableDirection; //direction of the node where pacman is nearer
-                    minDistance = distance; //their distance or the distance of node to pacman
+                    direction = availableDirection; //Current Direction to Best Direction
+                    minDistance = distance; //Update the distance
                 }
             }
 
-            this.ghost.movement.SetDirection(direction); // the ghost will move to the node
+            this.ghost.movement.SetDirection(direction); // the ghost will move to pacman and chase it
 
         }
     }
--- Assets/Scripts/GhostSpawn.cs
+++ Assets/Scripts/GhostSpawn.cs
@@ -4,15 +4,47 @@ using UnityEngine;
 
 public class GhostSpawn : GhostBehavior
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Transform insideTransform;
+    [SerializeField] private Transform outsideTransform;
+
+    private void OnDisable()
     {
-        
+        StartCoroutine(ExitTransition());
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator ExitTransition()
     {
-        
+        this.ghost.movement.SetDirection(Vector2.up, true);
+        this.ghost.movement.rg2D.isKinematic = true;
+        this.ghost.movement.enabled = false;
+
+        Vector3 position = this.transform.position;
+
+        float duration = 0.5f;
+        float elapsed = 0f;
+
+        while (elapsed < duration)
+        {
+            Vector3 newPosition = Vector3.Lerp(position, this.insideTransform.position, elapsed/duration);
+            newPosition.z = position.z;
+            this.ghost.transform.position = newPosition;
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        elapsed = 0f;
+
+        while (elapsed < duration)
+        {
+            Vector3 newPosition = Vector3.Lerp(this.insideTransform.position, this.outsideTransform.position, elapsed / duration);
+            newPosition.z = position.z;
+            this.ghost.transform.position = newPosition;
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        this.ghost.movement.SetDirection(new Vector2(Random.value <  0.5f ? -1f:1f, 0f), true);
+        this.ghost.movement.rg2D.isKinematic = false;
+        this.ghost.movement.enabled = true;
     }
 }
